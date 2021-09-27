10c08c69a30b86c44fdcd18c680d5de6deb6bb2d
jumpert <juan.perezc@correo.ucu.edu.uy>
2021-09-27 20:04:11 -0300

    Solved

--- src/Library/Greeter.cs
+++ src/Library/Greeter.cs
@@ -1,28 +1,10 @@
 ﻿using System;
 
+
 namespace Library
 {
-    public class Greeter
+    public class Greeter 
     {
-        private Greeter()
-        {
-            // Intentionally left blank
-        }
-
-        private static Greeter instance;
-
-        public static Greeter Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new Greeter();
-                }
-
-                return instance;
-            }
-        }
 
         public void SayHiToTheWorld()
         {
